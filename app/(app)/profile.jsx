0bcdccc1e00cc45af7c29d88ec272f9907476f88
Jhair Paris <70655025+jhairparis@users.noreply.github.com>
2023-10-18 17:50:37 -0500

    add login api

--- app/(app)/profile.jsx
+++ app/(app)/profile.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { FAB, Text } from "react-native-paper";
 import CustomView from "@/components/CustomView";
 import { useSession } from "@/components/providers/SessionProvider";
 
 const Profile = () => {
-  const { session } = useSession();
+  const { session, signOut } = useSession();
+
   return (
     <CustomView>
       <Text variant="displayLarge">Profile</Text>
-      <Text variant="labelMedium">The session: {session}</Text>
+      <Text variant="labelMedium">Name: {session.firstName}</Text>
+      <Text variant="labelMedium">Email: {session.email}</Text>
+
+      <FAB
+        icon="logout"
+        onPress={signOut}
+        style={{
+          position: "absolute",
+          margin: 16,
+          right: 0,
+          bottom: 0,
+        }}
+      />
     </CustomView>
   );
 };
--- components/providers/SessionProvider.jsx
+++ components/providers/SessionProvider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import useStorageState from "@/hooks/useStorageState";
 
 const AuthContext = React.createContext(null);
+const url = process.env.EXPO_PUBLIC_API_URL;
 
 export function useSession() {
   const value = React.useContext(AuthContext);
@@ -25,10 +26,48 @@ export function SessionProvider(props) {
       email === "" ||
       password === ""
     ) {
-      return Promise.reject();
+      return Promise.reject("Invalid credentials");
     }
 
-    setSession(email);
+    const csrf = await fetch(`${url}/api/auth/csrf`);
+    let csrfToken = await csrf.json();
+    csrfToken = csrfToken.csrfToken;
+
+    const dataForm = new URLSearchParams();
+    dataForm.append("email", email);
+    dataForm.append("password", password);
+    dataForm.append("csrfToken", csrfToken);
+    dataForm.append("json", true);
+
+    const res = await fetch(`${url}/api/auth/callback/credentials`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      body: dataForm.toString(),
+    });
+
+    const cookie = res.headers.get("set-cookie");
+
+    if (!res.ok && cookie === null) {
+      return Promise.reject("Invalid credentials");
+    }
+
+    const res2 = await fetch(`${url}/api/auth/session`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        cookie,
+      },
+    });
+
+    const data = await res2.json();
+
+    if (data.expires === undefined) {
+      return Promise.reject("Invalid credentials");
+    }
+
+    setSession(data.user);
     return Promise.resolve();
   };
 
